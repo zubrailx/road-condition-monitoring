79b941237c3dc91e06fbab9d0313987689465cb8
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-03-31 16:38:02 +0300

    mobile: add user current location marker

--- mobile/lib/widgets/map.dart
+++ mobile/lib/widgets/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:mobile/app/theme.dart';
 import 'package:mobile/state/gps.dart';
@@ -14,6 +15,16 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   late final MapController _mapController;
+
+  Marker _getUserMarker(LatLng mapPoint) {
+    return Marker(
+        point: mapPoint,
+        child: Image.asset("assets/svg/UserMap.png", width: 32),
+        height: 50,
+        width: 50,
+        rotate: true,
+        alignment: Alignment.center);
+  }
 
   @override
   void initState() {
@@ -44,6 +55,14 @@ class _MapWidgetState extends State<MapWidget> {
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.flutter_map_example',
+              ),
+              MarkerLayer(
+                markers: [
+                 _getUserMarker(LatLng(record.latitude!, record.longitude!)),
+                ]
+              ),
+              const MarkerLayer(
+                  markers: []
               ),
             ],
           );
