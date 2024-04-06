6aa571b4bd5bc67a16502cdadd16615031718657
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-06 16:23:34 +0300

    docker: add mqtt-receiver dockerfile

--- mqtt_receiver/main.py
+++ mqtt_receiver/main.py
@@ -1,7 +1,17 @@
 import paho.mqtt.client as mqtt
+import sys
+
 from paho.mqtt.enums import CallbackAPIVersion
 
+
+if (len(sys.argv) < 3):
+    print('host args are not passed')
+    exit(-1);
+
 TOPIC = "monitoring"
+
+MQTT_HOST = sys.argv[1]
+MQTT_PORT = int(sys.argv[2])
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
@@ -16,6 +26,6 @@ mqttc = mqtt.Client(CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-mqttc.connect("localhost", 1883, 60)
+mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
 
 mqttc.loop_forever()
