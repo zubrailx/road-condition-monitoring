292e50a450e776842858206f78d50f37b279d770
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-07 22:41:33 +0300

    guessr: pretty print log info

--- guessr/main.py
+++ guessr/main.py
@@ -1,11 +1,25 @@
 import sys
 from kafka import KafkaConsumer
+from datetime import datetime
 
 import common.model.gen.monitoring.monitoring_pb2 as monitoring
 
 TOPIC = "monitoring"
 GROUP_ID = "guessr-group"
 AUTO_OFFSET_RESET = "latest"
+
+def kafka_to_timestamp(date):
+# .replace(microsecond=date%1000*1000)
+    return datetime.utcfromtimestamp(date//1000);
+
+def get_pretty_kafka_log(message, data: monitoring.Monitoring):
+    string = f"NETWORK[{time}]: Topic '{TOPIC}', offset: {message.offset}, size: {len(message.value)} bytes." 
+    string += f"\n\tAccount id: {data.account.accound_id}, name: {data.account.name}."
+    string += f"\n\tAccelerometer: {len(data.accelerometer_records)} recs"
+    string += f"\n\tGyroscope: {len(data.gyroscope_records)} recs"
+    string += f"\n\tGps: {len(data.gps_records)} recs"
+    return string
+
 
 if len(sys.argv) < 2:
     print("host args are not passed")
@@ -22,6 +36,8 @@ consumer = KafkaConsumer(
 )
 
 for message in consumer:
+    time = kafka_to_timestamp(message.timestamp);
     data = monitoring.Monitoring()
     data.ParseFromString(message.value)
-    print("Offset:", message.offset)
+    print(get_pretty_kafka_log(message, data))
+
