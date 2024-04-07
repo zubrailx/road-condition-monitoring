570f7fb78b3896a7fcb6180f4a58596766d28cd1
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-07 20:33:56 +0300

    guessr: change consumer group

--- guessr/main.py
+++ guessr/main.py
@@ -7,10 +7,10 @@ consumer = KafkaConsumer(
     bootstrap_servers=["localhost:9092"],
     auto_offset_reset="latest",
     enable_auto_commit=True,
-    group_id="test-consumer-group",
+    group_id="guessr-group",
 )
 
 for message in consumer:
     data = monitoring.Monitoring()
     data.ParseFromString(message.value)
-    print(data)
+    print('Offset:', message.offset)
