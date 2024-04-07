15a5f2a80eccd5ae036cfbc856c67a575cdcdc58
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-04-07 21:56:32 +0300

    docker: add guessr to docker-compose, read params from input

--- guessr/main.py
+++ guessr/main.py
@@ -1,16 +1,27 @@
+import sys
 from kafka import KafkaConsumer
 
 import common.model.gen.monitoring.monitoring_pb2 as monitoring
 
+TOPIC = "monitoring"
+GROUP_ID = "guessr-group"
+AUTO_OFFSET_RESET = "latest"
+
+if len(sys.argv) < 2:
+    print("host args are not passed")
+    exit(-1)
+
+BOOTSTRAP_SERVERS = sys.argv[1] # localhost:9092
+
 consumer = KafkaConsumer(
-    "monitoring",
-    bootstrap_servers=["localhost:9092"],
-    auto_offset_reset="latest",
+    TOPIC,
+    bootstrap_servers=[BOOTSTRAP_SERVERS],
+    auto_offset_reset=AUTO_OFFSET_RESET,
     enable_auto_commit=True,
-    group_id="guessr-group",
+    group_id=GROUP_ID,
 )
 
 for message in consumer:
     data = monitoring.Monitoring()
     data.ParseFromString(message.value)
-    print('Offset:', message.offset)
+    print("Offset:", message.offset)
